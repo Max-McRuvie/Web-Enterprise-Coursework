d788aba6d5015bfc55c8be4514bfe5e42df2de47
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-06 12:18:51 +0000

    Authorisation error handler

--- server/server.js
+++ server/server.js
@@ -24,6 +24,15 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use("/", userRoutes)
 app.use("/", authRoutes)
 
+// Authorisation error handler
+app.use((err, req, res, next) => {
+    if (err.name === 'UnauthorizedError') {
+        res.status(401).json({"error": err.name + ": " + err.message});
+    }else if(err){
+        res.status(400).json({"error": err.name + ": " + err.message});
+    }
+});
+
 // 404 not found
 app.use((req, res, next) => {
     res.send("Page not found!")

95a668ddd85697b04539431735c079998f8b1f28
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-06 02:42:46 +0000

    Password encryption

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,7 +1,7 @@
 import mongoose from 'mongoose';
 import crypto from 'crypto';
 
-
+// User Schema
 const UserSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -27,6 +27,48 @@ const UserSchema = new mongoose.Schema({
         default: Date.now
     }
 })
+
+// Virtual Fields
+UserSchema.virtual('password')
+    .set((password) => {
+        this._password = password
+        this.salt = this.makeSalt()
+        this.hashed_password = this.encryptPassword(password)
+    })
+    .get(() => {
+        return this._password
+    })
+
+// Methods
+UserSchema.methods = {
+    authenticate: function(plainText) {
+        return this.encryptPassword(plainText) === this.hashed_password
+    },
+    encryptPassword: function(password) {
+        if(!password) return ''
+        try {
+            return crypto
+                .createHmac('sha1', this.salt)
+                .update(password)
+                .digest('hex')
+        } catch (err) {
+            return ''
+        }
+    },
+    makeSalt: () => {
+        return Math.round((new Date().valueOf() * Math.random())) + ''
+    }
+}
+
+// Validation
+UserSchema.path('hashed_password').validate(function(v) {
+    if (this._password && this._password.length < 6) {
+        this.invalidate('password', 'Password must be at least 6 characters.')
+    }
+    if (this.isNew && !this._password) {
+        this.invalidate('password', 'Password is required')
+    }
+}, null)
 
 const userModel = mongoose.model('User', UserSchema);
 userModel.createIndexes();

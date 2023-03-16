fc988c2663a93518e69fc6bf4fdb1f005d2548dc
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-16 21:42:09 +0000

    Uneeded code

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import './App.css';
-import MainRouter from './MainRouter';
-import {BrowserRouter} from "react-router-dom";
 
 function App() {
   return (
-    <BrowserRouter>
-      <MainRouter />
-    </BrowserRouter>
+    <>
+      <h1>React Router</h1>
+    </>
   );
 }
 

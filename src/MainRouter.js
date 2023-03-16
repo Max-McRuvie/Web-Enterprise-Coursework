82a3f369f25fb77230ed60fbeb176ae06ec41550
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-16 15:12:58 +0000

    Work around for useRouteError

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -5,12 +5,14 @@ import {
 } from "react-router-dom";
 
 import Home from './pages/Home';
+import ErrorPage from './pages/ErrorPage';
 
 const MainRouter = () => {
     return (
         <>
             <Routes>
                 <Route path="/" element={<Home />} />
+                <Route path="*" element={<ErrorPage />} />
             </Routes>
         </>
     )

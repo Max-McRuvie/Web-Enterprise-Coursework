394c77baab70fd94a93c60f664f6adfb540f1ff7
max mcruvie <mcruvie@hotmail.co.uk>
2023-03-23 21:11:07 +0000

    Root basic styling

--- src/router/root.js
+++ src/router/root.js
@@ -1,22 +1,43 @@
 import React from 'react';
 import { Outlet } from "react-router-dom";
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import theme from '../theme';
+import { ThemeProvider } from '@mui/material/styles';
+const { main, darkNavbar, light, contrastText } = theme.palette.primary;
+
 
 export default function Root() {
     return (
-        <>
-            <div id="sidebar">
-                <h1>Menu</h1>
-                <div>
-                <a href={'/login'}>Login</a>
-                </div>
-                <div>
-                <a href={'/signUp'}>Sign up</a>
-                </div>
-            </div>
-            <br></br>
-            <div id="detail">
+        <ThemeProvider theme={theme}>
+            <Box sx={{ flexGrow: 1 }}>
+                <AppBar position="static" sx={{background: darkNavbar}}>
+                    <Toolbar sx={{justifyContent: 'space-between'}}>
+                        <div styles={{direction:"row"}}>
+                            {/* <IconButton
+                                size="large"
+                                edge="start"
+                                color="inherit"
+                                aria-label="menu"
+                                sx={{ mr: 2 }}
+                            >
+                                <MenuIcon />
+                            </IconButton> */}
+                            <Button color="inherit" href='/'>Home</Button>
+                        </div>
+                        <div styles={{direction:"row"}}>
+                            <Button color="inherit" href='/login'>Login</Button>
+                            <Button color="inherit" href='/signup'>Sign Up</Button>
+                        </div>
+                    </Toolbar>
+                </AppBar>
                 <Outlet />
-            </div>
-        </>
+            </Box>
+        </ThemeProvider>
     );
 }

e05a8f976d4ee52220459cabc173adc9eecaf209
max mcruvie <mcruvie@hotmail.co.uk>
2023-04-05 01:11:27 +0100

    Removed hasAuth due to errors on quote route use

--- server/routes/quote.routes.js
+++ server/routes/quote.routes.js
@@ -6,21 +6,20 @@ import quoteCtrl from '../controllers/quote.controller.js';
 const router = express.Router(); // eslint-disable-line new-cap
 
 router.route('/api/calculation')
-    .post(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.calculateQuote);
+    .post(authCtrl.requireSignin, quoteCtrl.calculateQuote);
 
 router.route('/api/quotes')
-    .post(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.createQuote);
+    .post(authCtrl.requireSignin, quoteCtrl.createQuote);
     
-
 router.route('/api/:userId/quotes')
-    .get(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.listQuotes)
-    .delete(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.removeQuote);
+    .get(authCtrl.requireSignin, quoteCtrl.listQuotes)
+    .delete(authCtrl.requireSignin, quoteCtrl.removeQuote);
 
 router.route('/api/quotes/:quoteId')
-    .get(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.getQuoteByID)
+    .get(authCtrl.requireSignin, quoteCtrl.getQuoteByID)
     .put(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.updateQuote);
 
 router.route('/api/:userId/quotes/combine')
-    .post(authCtrl.requireSignin, authCtrl.hasAuthorization, quoteCtrl.combineQuotes)
+    .post(authCtrl.requireSignin, quoteCtrl.combineQuotes)
     
 export default router;

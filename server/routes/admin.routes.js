b22cdcaedaaa89dc8d69050a39ed5fd4d6ddf193
max mcruvie <mcruvie@hotmail.co.uk>
2023-04-05 04:54:48 +0100

    Adjustment to admin routes

--- server/routes/admin.routes.js
+++ server/routes/admin.routes.js
@@ -4,10 +4,11 @@ import adminCtrl from '../controllers/admin.controller.js';
 
 const router = express.Router();
 
-route.route('/api/admin/paygrade')
+route.route('/api/admin/paygrade/:userId')
     .update(authCtrl.requireSignin, authCtrl.hasAdminAuthorization, adminCtrl.updatePaygrade);
 
-router.route('api/admin/fudge-factor')
+router.route('api/admin/fudge-factor/:userId')
     .update(authCtrl.requireSignin, authCtrl.hasAdminAuthorization, adminCtrl.updateFudgeFactor);
 
+router.param('userId', userCtrl.userByID);
 
